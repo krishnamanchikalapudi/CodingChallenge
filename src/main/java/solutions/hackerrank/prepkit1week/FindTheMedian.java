11565a5b3ef2d1c9db0a1246977be299f6e103c0
Krishna <krishna.manchikalapudi@gmail.com>
2024-01-19 12:50:40 -0800

    1 week interview prep kit

--- src/main/java/solutions/hackerrank/prepkit1week/FindTheMedian.java
+++ src/main/java/solutions/hackerrank/prepkit1week/FindTheMedian.java
@@ -2,8 +2,11 @@ package solutions.hackerrank.prepkit1week;
 
 import java.util.Comparator;
 import java.util.List;
+
 /**
- * The median of a list of numbers is essentially its middle element after sorting. The same number of elements occur after it as before. Given a list of numbers with an odd number of elements, find the median?
+ * The median of a list of numbers is essentially its middle element after
+ * sorting. The same number of elements occur after it as before. Given a list
+ * of numbers with an odd number of elements, find the median?
  * 
  * Example 1:
  * Input: arr=[5,3,1,2,4]
@@ -14,15 +17,15 @@ import java.util.List;
  * Output: Median is 3
  */
 public class FindTheMedian {
-    public static int solution(List<Integer> arr) {
+    public int solution(List<Integer> arr) {
         int k = 0;
         arr.sort(Comparator.naturalOrder());
         int n = arr.size();
-        if (n % 2 == 0) 
+        if (n % 2 == 0)
             k = (arr.get(n / 2) + arr.get(n / 2 - 1)) / 2;
-        else 
+        else
             k = arr.get(n / 2);
-        
+
         return k;
     }
 }
